39b9f5d57411bf6dc18178a1ee99565c29a80664
Thomas159753 <99398525+Thomas159753@users.noreply.github.com>
2022-09-03 17:36:47 +1000

    test

--- java.js
+++ java.js
@@ -6,89 +6,91 @@ const eraser = document.getElementById('eraser');
 const shadingButton = document.querySelector("#checkbox");
 let firstGrid = 0;
 let shading = false;
+let usermode = 'pencil';
+// buttons seting modes
 
-// ------------------------- shadinmg toggle button
-shadingButton.addEventListener('click', function(){
-  if (shading === false ) {
-  shading = true;
-  }
-  else {
-  shading = false;
-  }
+pencil.addEventListener('click', function(){
+  usermode = 'pencil'
 })
 
-// ------------------------ creates divs button
+eraser.addEventListener('click', function(){
+  usermode = 'eraser'
+})
+
 btn.addEventListener('click', function() {
   
   const userInput = document.getElementById('userInput').value;
-  //   unnecessary this but im keeping it just in case
-  if(userInput > 100){ //
-    alert("Maximu pixels is 100");// unnecessary but im keeping it for failsafe
-  } //
 
   if(firstGrid === 0 && userInput < 100) {
     createDivs(userInput);
     firstGrid++
+    return firstGrid;
   }
   
   if(userInput < 100) {
-    const removeDivs = document.querySelectorAll('div.pixels');
+    const removeDivs = document.querySelectorAll('.pixels');
     removeDivs.forEach(removeDivs => {
       workspace.removeChild(removeDivs)
     })
     createDivs(userInput);
   }
 })
-// ---------------------function that makes the grids
+
+// create divs faction
 function createDivs (userInput){
   for (let i = 0; i < (userInput * userInput); i++) {
     const divs = document.createElement('div');
     divs.classList.add('pixels')
     workspace.appendChild(divs);
   }
-  // ---------------------grid div placement
-
   workspace.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
   workspace.style.gridTemplateRows = `repeat(${userInput}, 1fr)`;
-  // --------------------coloring grids
- 
-  pencil.addEventListener('click', function(){;
-
-    if(shading == true) {
-      
-      const gridDivs = document.querySelectorAll('div.pixels');
-      gridDivs.forEach(gridDivs => {
-        gridDivs.addEventListener('click', function(){
-        gridDivs.style.backgroundColor = 'red';
-        })
-      })
-    }
-    
-    else{
-      const gridDivs = document.querySelectorAll('div.pixels');
-      gridDivs.forEach(gridDivs => {
-        gridDivs.addEventListener('click', function(){
-        gridDivs.style.backgroundColor = document.getElementById('colorpicker').value
-        })
-      })
-    } 
-  })
-
-    eraser.addEventListener('click', function(){;
-      const gridDivs = document.querySelectorAll('div.pixels');
-      gridDivs.forEach(gridDivs => {
-        gridDivs.addEventListener('click', function(){
-        gridDivs.style.backgroundColor = 'white';
-        })
-      })
-    })
+  const gridDivs = document.querySelectorAll('.pixels');
+  gridDivs.forEach(gridDivs => {
+    gridDivs.addEventListener('click', coloring)});
 }
-//---------------------display grid numbers
+
+// coloring function
+
+function coloring(){
+  if(usermode === 'pencil'){  
+  this.style.backgroundColor = document.getElementById('colorpicker').value;
+  console.log(usermode);}
+
+  else if(usermode === 'eraser'){
+    this.style.backgroundColor = 'white';
+  }
+
+  else if(usermode === 'shading'){
+    this.style.opacity = '0.5';
+  }
+}
+// display grid numbers
 
 userInput.addEventListener('input', updateValue);
 
 function updateValue(e) {
   gridDisplay.textContent = `Your grid is ${e.target.value} x ${e.target.value}`;
 }
+
+// shadinmg toggle button
+shadingButton.addEventListener('click', function(){
+  if (shading === false ) {
+  shading = true;
+  usermode = 'shading';
+  }
+  else {
+  shading = false;
+  }
+})
+//gridDivs.forEach(gridDivs => {
+//gridDivs.addEventListener('click', coloring(pencil, eraser))})
+
+
+
+
+
+
+
 
   
